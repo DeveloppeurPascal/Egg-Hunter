ad513d3a559b86c7751f18733d09ccbebf9425c9
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2024-08-05 20:06:57 +0200

    added copyright comments on Pascal files

--- src-game/uVersionDuProgramme.pas
+++ src-game/uVersionDuProgramme.pas
@@ -1,3 +1,44 @@
+﻿/// <summary>
+/// ***************************************************************************
+///
+/// Egg Hunter
+///
+/// Copyright 2021-2024 Patrick Prémartin under AGPL 3.0 license.
+///
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+/// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+/// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+/// DEALINGS IN THE SOFTWARE.
+///
+/// ***************************************************************************
+///
+/// Egg Hunter is an RPG sprinkled with duck breeding.
+///
+/// You have to harvest their eggs, incubate them until they hatch, and
+/// explode production quotas (just for fun).
+///
+/// This repository contains the game source and its level editor.
+///
+/// ***************************************************************************
+///
+/// Author(s) :
+/// Patrick PREMARTIN
+///
+/// Site :
+/// https://egghunter.gamolf.fr/
+///
+/// Project site :
+/// https://github.com/DeveloppeurPascal/Egg-Hunter
+///
+/// ***************************************************************************
+/// File last update : 2024-08-05T19:36:12.387+02:00
+/// Signature : f252d23bdc615e03a4e571bf4c142e4e85776736
+/// ***************************************************************************
+/// </summary>
+
 unit uVersionDuProgramme;
 
 interface
